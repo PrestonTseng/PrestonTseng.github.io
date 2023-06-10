050e83f9dfae18fa214cb44f2a3854218c55585c
JackyTseng <csps50404@gmail.com>
2023-06-11 00:06:29 +0800

    add i18n

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div>
+        <NuxtWelcome />
+        {{ $t('hello') }}
+    </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,55 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  typescript: {
-    typeCheck: true
-  },
-  modules: [
-    ['@invictus.codes/nuxt-vuetify', {
-      /* vuetify options */
-      vuetifyOptions: {
-        // @TODO: list all vuetify options
-      },
-  
-      moduleOptions: {
-        /* nuxt-vuetify module options */
-        treeshaking: true ,
-        useIconCDN: true ,
-  
-        /* vite-plugin-vuetify options */
-        styles: true,
-        autoImport: true ,
-      }
-    }]
-  ],
+    devtools: { enabled: true },
+    typescript: {
+        typeCheck: true
+    },
+    modules: [
+        [
+            '@invictus.codes/nuxt-vuetify',
+            {
+                /* vuetify options */
+                vuetifyOptions: {
+                    // @TODO: list all vuetify options
+                },
+
+                moduleOptions: {
+                    /* nuxt-vuetify module options */
+                    treeshaking: true,
+                    useIconCDN: true,
+
+                    /* vite-plugin-vuetify options */
+                    styles: true,
+                    autoImport: true
+                }
+            }
+        ],
+        [
+            '@nuxtjs/i18n',
+            {
+                langDir: 'locales',
+                locales: [
+                    {
+                        code: 'en',
+                        iso: 'en-US',
+                        file: 'en.json',
+                        name: 'English'
+                    },
+                    {
+                        code: 'zh-TW',
+                        iso: 'zh-TW',
+                        file: 'zh-TW.json',
+                        name: '繁體中文'
+                    }
+                ],
+                strategy: 'no_prefix',
+                defaultLocale: 'en',
+                detectBrowserLanguage: {
+                    useCookie: true,
+                    cookieKey: 'i18n_redirected',
+                    redirectOn: 'root' // recommended
+                }
+            }
+        ]
+    ]
 })
