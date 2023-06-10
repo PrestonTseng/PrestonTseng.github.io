38564fd515e8e676741497b4951e089456ed3bc4
JackyTseng <csps50404@gmail.com>
2023-06-10 21:36:01 +0800

    add vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,23 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   typescript: {
     typeCheck: true
-  }
+  },
+  modules: [
+    ['@invictus.codes/nuxt-vuetify', {
+      /* vuetify options */
+      vuetifyOptions: {
+        // @TODO: list all vuetify options
+      },
+  
+      moduleOptions: {
+        /* nuxt-vuetify module options */
+        treeshaking: true ,
+        useIconCDN: true ,
+  
+        /* vite-plugin-vuetify options */
+        styles: true,
+        autoImport: true ,
+      }
+    }]
+  ],
 })
