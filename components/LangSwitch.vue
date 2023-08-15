eab53b5dffe26147bf3dd0469da1e97b6a1a2a8c
JackyTseng <csps50404@gmail.com>
2023-08-15 23:41:33 +0800

    update lang switch

--- components/LangSwitch.vue
+++ components/LangSwitch.vue
@@ -1,26 +1,30 @@
-<script setup>
-import { useI18n } from 'vue-i18n'
-
-const { locale, locales, setLocale } = useI18n()
-
-const availableLocales = computed(() => {
-  return locales.value.filter((i) => i.code !== locale.value)
-})
-</script>
-
 <template>
   <v-menu open-on-hover location="bottom">
     <template v-slot:activator="{ props }">
-      <v-btn v-bind="props" variant="plain" icon="mdi-google-translate" :ripple="false"> </v-btn> 
+      <v-btn v-bind="props" width="92" :ripple="false">{{ currentLocaleName }} </v-btn>
     </template>
     <v-list>
       <v-list-item
         v-for="locale in availableLocales"
         :key="locale.code"
-        @click.stop="setLocale(locale.code)"
+        @click="setLocale(locale.code)"
       >
         <v-list-item-title>{{ locale.name }}</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-menu>
 </template>
+
+<script setup>
+import { useI18n } from "vue-i18n";
+
+const { locale, locales, setLocale } = useI18n();
+
+const availableLocales = computed(() => {
+  return locales.value.filter((i) => i.code !== locale.value);
+});
+
+const currentLocaleName = computed(
+  () => locales.value.find((i) => i.code == locale.value)?.name
+);
+</script>
