e7534612e28ad67b6fe2b9c39b206d8b03b94e8b
JackyTseng <csps50404@gmail.com>
2023-08-17 23:58:59 +0800

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    baseURL: '/',
+    buildAssetsDir: 'assets'
+  },
   devtools: { enabled: true },
   typescript: {
     typeCheck: true
