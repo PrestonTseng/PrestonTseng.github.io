e34ccff715d18e45d86e5ce7efc174bfc90b8b4c
JackyTseng <csps50404@gmail.com>
2023-08-18 00:53:37 +0800

    fixed svg path

--- components/about/Skills.vue
+++ components/about/Skills.vue
@@ -23,8 +23,8 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import { useI18n } from 'vue-i18n'
-import pytorchSvg from '~/assets/icons/pytorch.svg'
-import openAiSvg from '~/assets/icons/openai.svg'
+import pytorchSvg from '../assets/icons/pytorch.svg'
+import openAiSvg from '../assets/icons/openai.svg'
 
 const { t } = useI18n({
   messages: {
