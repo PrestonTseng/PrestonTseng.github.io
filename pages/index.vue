99a6cf4b085fc75087ddb87a02e98d258fad7453
JackyTseng <csps50404@gmail.com>
2023-10-07 20:55:12 +0800

    make cv downloadable

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container id="about-containter" style="max-width: 1290px">
+  <v-container id="about-containter" style="max-width: 1280px">
     <v-row align="start" justify="center">
       <v-col cols="12" style="height: 50px"></v-col>
       <v-col cols="12" lg="4" class="text-center">
@@ -26,7 +26,7 @@
               <v-icon left :icon="link.icon" class="px-1 pr-2"></v-icon>
               <span class="text-capitalize">{{ link.text }}</span>
             </v-btn>
-            <v-btn size="small" color="secondary" variant="outlined" :ripple="false" class="rounded-pill ml-4" style="opacity: 0.7" @click="print">
+            <v-btn size="small" color="secondary" variant="outlined" :ripple="false" class="rounded-pill ml-4 d-print-none" style="opacity: 0.7" @click="print">
               <v-icon left icon="mdi-download" class="px-1 pr-2"></v-icon>
               <span class="text-capitalize">{{ t('download') }}</span>
             </v-btn>
@@ -35,18 +35,18 @@
       </v-col>
       <v-col cols="12" style="height: 50px"></v-col>
       <v-col cols="12" lg="6">
-        <v-col cols="12" class="print-full-height">
+        <v-col cols="12">
           <about-experiences></about-experiences>
         </v-col>
       </v-col>
       <v-col cols="12" lg="6">
-        <v-col cols="12" class="print-full-height">
+        <v-col cols="12">
           <about-certifications></about-certifications>
         </v-col>
-        <v-col cols="12" class="print-full-height">
+        <v-col cols="12">
           <about-skills></about-skills>
         </v-col>
-        <v-col cols="12" class="print-full-height">
+        <v-col cols="12">
           <about-education></about-education>
         </v-col>
       </v-col>
@@ -60,6 +60,8 @@ import { useI18n } from 'vue-i18n'
 import { useDisplay } from 'vuetify'
 import headshotLg from '@/public/images/profile_photo.png'
 import headshot from '@/public/images/profile_photo_sm.png'
+import html2canvas from 'html2canvas'
+import { jsPDF } from 'jspdf'
 
 const { smAndUp, mdAndUp, lgAndUp, xlAndUp } = useDisplay()
 
@@ -69,13 +71,15 @@ const { t } = useI18n({
       prologue: 'Po Yen Tseng',
       shortIntro: 'Full-Stack Developer',
       longIntro: `With 4 years of front-end experience, I transitioned to a full-stack role a year ago, specializing in Vue.js, Nuxt.js (front-end), and .NET Core with MySQL (back-end). I focus on developing web services for architecture and civil engineering, such as transferring complex BIM models to the web for improved accessibility and collaboration. I also explore tools like Azure OpenAI and Cognitive Search to enhance document retrieval and analysis. My aim is to innovate and drive digital transformation in this field.`,
-      download: 'Download'
+      download: 'Download',
+      fileName: `Preston's CV`
     },
     'zh-tw': {
       prologue: '曾柏硯',
       shortIntro: '全端工程師',
       longIntro: `從高中開始，我便持續自學網頁開發，至今已有４年前端工作經驗．１年前因現職工作需求，開始涉獵後端，作為全端工程師，同時負責前後端的開發。前端方面主要使用 Vue.js 和 Nuxt.js 進行開發。而後端方面則採用 .NET Core 搭配 MySQL。 在目前的職務上致力於研發各種網路服務，為建築和土木工程領域提供數位轉型的解決方案。例如將原本需要高度圖像處理設備的 BIM 模型轉移到網頁上，打破了設備的限制，讓各種不同的設備和環境都能立即查看和協作。此外，我也持續學習並嘗試新工具，例如運用 Azure OpenAI 和 Cognitive Search 技術，讓過去的建築和土木工程文件以及工程圖紙能夠輕鬆地被檢索和分析。我的使命是不斷探索創新的解決方案，推動建築與土木工程領域的數位化進程。`,
-      download: '下載'
+      download: '下載',
+      fileName: `曾柏硯的履歷`
     }
   }
 })
@@ -118,27 +122,33 @@ const links = computed(() => [
 ])
 
 const print = () => {
-  const WinPrint = window.open(`${window.location.href}`, '', 'left=0,top=0,width=1280,height=1440,toolbar=0,scrollbars=0,status=0')
-  WinPrint!.focus()
-  WinPrint.print()
-}
+  var element = document.getElementById('about-containter')
+  html2canvas(element as HTMLElement, { windowWidth: 1280, windowHeight: 1440, scale: 3, useCORS: true, allowTaint: true, ignoreElements: (e) => e.classList.contains('d-print-none') }).then((canvas) => {
+    var pdf = new jsPDF('p', 'mm', 'a4')
+    var img = canvas.toDataURL('image/jpeg', 1.0)
 
-// onMounted(() => {
-//   if (window.location.search) {
-//     setTimeout(() => {
-//       // to let vuetify have time to render colors
-//       document.title = this.purchaseOrder.orderNumber
-//       window.print()
-//       window.close()
-//     }, WAITING_TIME_FOR_PRINT)
-//   }
-// })
+    var widthOffset = 0
+    var heightOffset = 0
+    var width = pdf.internal.pageSize.getWidth()
+    var height = pdf.internal.pageSize.getHeight()
+
+    if (canvas.height / canvas.width > height / width) {
+      width = (height * canvas.width) / canvas.height
+      widthOffset = (pdf.internal.pageSize.getWidth() - width) / 2
+    } else {
+      height = (width * canvas.height) / canvas.width
+      heightOffset = (pdf.internal.pageSize.getHeight() - height) / 2
+    }
+
+    pdf.addImage(img, 'JPEG', widthOffset, heightOffset, width, height)
+    pdf.save(t('fileName'))
+  })
+}
 </script>
 
 <style lang="scss">
 @page {
   size: A4;
   -webkit-print-color-adjust: exact;
-  resolution: 300dpi;
 }
 </style>
